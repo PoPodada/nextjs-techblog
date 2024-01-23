e727f3e629d0f935b494bdc27d7f277f3c6105b6
PoPodada <goito.722@icloud.com>
2024-01-23 16:07:19 +0900

    add parser

--- src/builder/__tests__/memberPosts.spec.ts
+++ src/builder/__tests__/memberPosts.spec.ts
@@ -1,1 +1,14 @@
-describe("memberPost")
+
+import { title } from "process"
+import { parseRSSfeed } from "../memberPosts"
+
+describe("membersPost",()=>{
+    test("sample",async()=>{
+        const results = await parseRSSfeed('https://qiita.com/PoPodada/feed')
+
+        expect(results).toEqual([{
+            title:'自分のサイトをmysqlと紐づける',
+            url:'https://qiita.com/PoPodada/items/281f68e36abf927c70b4',
+        }])
+    })
+})
---
+++ src/builder/memberPosts.ts
@@ -0,0 +1,13 @@
+import Parser from 'rss-parser';
+
+const parser = new Parser()
+export const parseRSSfeed = async(url:string)=>{
+    const feed = await parser.parseURL(url)
+    return feed.items.map((item) =>{
+        return{
+            title:item.title,
+            url:item.link,
+        }
+    })[0]
+    
+}
